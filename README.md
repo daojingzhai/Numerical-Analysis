d6498ded153a6466d5fb3aee2008ec37300b3c6d
Daojing <daojing.zhai@gmail.com>
2018-04-27 20:48:47 +0800

    Add some programs.

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Numerical-Analysis
+1. Solving equations and systems of equations
+    - Solving system of linear equations
+        - Iterative methods
+            - Jacobi method
+            - Gauss-Seidel method
+            - Successive over-relaxation
+    
---
+++ Solving equations and systems of equations/Iterative methods/Jacobi Method/Jacobi.m
@@ -0,0 +1,21 @@
+function [x] = Jacobi(A,b,x0,err,Maxiter)
+% Ax = b
+% err is error
+% Maxiter is the maximum iteration times.
+D = diag(diag(A));%Find diagonal matrix of A
+L = -tril(A,-1);%Find lower triangular part of A
+U = -triu(A,1);%Find upper triangular part of A
+B = D\(L+U); 
+f = D\b;
+x = B*x0+f; %x = bx+f
+iter = 0;
+while norm(x-x0, inf)>=err  % we use inf-norm 
+    x0 = x;
+    x = B*x0+f;
+    iter = iter + 1;
+    if(iter>=Maxiter)
+        disp('Warning:Maximum number of iterations exceeded!')
+        return;
+    end
+end
+end
